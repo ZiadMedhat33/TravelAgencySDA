b241f8ee2199f562f8665d32e6513c606e12a9d8
ZiadMedhat33 <ziadmedhat301@gmail.com>
2024-12-16 18:28:44 +0200

    inter

---
+++ travelagency/src/main/java/com/travelagency/SpringController/SpringController.java
@@ -0,0 +1,10 @@
+package com.travelagency.SpringController;
+
+import com.travelagency.model.*;
+import com.travelagency.HotelManagment.*;
+import com.travelagency.LocalEventManagment.*;
+import org.springframework.web.bind.*;
+
+public class SpringController {
+
+}
