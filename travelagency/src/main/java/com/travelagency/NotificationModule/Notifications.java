18d67fe5467a6c7e0bb4e6687e82f88ad4325cf7
Yousef <yousefsmn@gmail.com>
2024-12-21 20:16:33 +0200

    Notifications functions

--- travelagency/src/main/java/com/travelagency/NotificationModule/Notifications.java
+++ travelagency/src/main/java/com/travelagency/NotificationModule/Notifications.java
@@ -17,6 +17,22 @@ public class Notifications {
         }
         return chosenNotifications;
     }
+    public ArrayList<Notification> getUserSuccessfulNotifications(String userid) {
+        ArrayList<Notification> chosenNotifications = new ArrayList<>();
+        for (Notification notification : notifications) {
+            if (notification.getOverallStatus() && notification.getUserid() == userid)
+                chosenNotifications.add(notification);
+        }
+        return chosenNotifications;
+    }
+    public ArrayList<Notification> getUserTypeSuccessfulNotifications(String type, String userid) {
+        ArrayList<Notification> chosenNotifications = new ArrayList<>();
+        for (Notification notification : notifications) {
+            if (notification.getOverallStatus() && notification.getType() == type && notification.getUserid() == userid)
+                chosenNotifications.add(notification);
+        }
+        return chosenNotifications;
+    }
     public ArrayList<Notification> getTypeSuccessfulNotifications(String type) {
         ArrayList<Notification> chosenNotifications = new ArrayList<>();
         for (Notification notification : notifications) {
