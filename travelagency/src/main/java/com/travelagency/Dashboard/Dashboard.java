e9b59b9f423b36f00fba058cbbc4ef9092b41324
Marwan Ahmed <7marawan7@gmail.com>
2024-12-21 21:41:26 +0200

    Dashboard V7

--- travelagency/src/main/java/com/travelagency/Dashboard/Dashboard.java
+++ travelagency/src/main/java/com/travelagency/Dashboard/Dashboard.java
@@ -3,6 +3,6 @@ import com.travelagency.NotificationModule.Notifications;
 import com.travelagency.model.*;
 public class Dashboard extends DashboardAbstract{
   public Dashboard( Model model,String userid, Notifications notificationGetter){
-    super(model,userid,notificationGetter);
+    super(model,userid);
   }
 }
