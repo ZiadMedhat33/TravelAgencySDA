31b0ef72011046845905f893eaf069da05203981
Marwan Ahmed <7marawan7@gmail.com>
2024-12-21 20:51:44 +0200

    .

--- travelagency/src/main/java/com/travelagency/Dashboard/Dashboard.java
+++
@@ -1,8 +0,0 @@
-package com.travelagency.Dashboard;
-import com.travelagency.NotificationModule.Notifications;
-import com.travelagency.model.*;
-public class Dashboard extends DashboardAbstract{
-  public Dashboard( Model model,String userid, Notifications notificationGetter){
-    super(model,userid,notificationGetter);
-  }
-}
